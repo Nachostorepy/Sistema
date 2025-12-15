4a7a37bce8867472cde9a39df14f77f05146f12f
Nachostorepy <ignaciomartinez771999@gmail.com>
2025-12-15 17:27:35 -0300

    Update Supabase URL and anon key

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 // Configuración de Supabase
-const SUPABASE_URL = 'https://ismdjpasduqxvynnhwms.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzbWRqcGFzZHVxeHZ5bm5od21zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MzMzNjgsImV4cCI6MjA3ODEwOTM2OH0.6fFh1vRGGobB4-XyQ_mgih0ak7hXy0L2EmVwiURRV5Q';
+const SUPABASE_URL = 'https://wyajqxzwlsruxpehivtq.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind5YWpxeHp3bHNydXhwZWhpdnRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MjkwODksImV4cCI6MjA4MTQwNTA4OX0.Sxs9ikJP9ryNY72PpspEM9vYiM_rG5LB3_UxLY9ZkqI';
 
 // Cliente de Supabase
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
